f017c29e5d6d2b547badc1434adb49a9fee55717
Mariella Gustafsson <mariella_gustafsson@hotmail.se>
2026-05-19 15:24:47 +0200

    Flyttat API anrop till game-view

--- src/components/layout/monster-section.tsx
+++ src/components/layout/monster-section.tsx
@@ -1,42 +1,14 @@
-import { useEffect, useState } from "react";
 import Monster from "../ui/monster";
 import type { Monster as MonsterType } from "../../types/monster";
-import type { MonsterAPI } from "../../types/monster";
 
 interface MonsterSectionProps {
   correctAnswer: boolean | null;
   dropId?: string | null;
   setCorrectAnswer?: React.Dispatch<React.SetStateAction<boolean | null>>;
+  monsters: MonsterType[];
 }
 
-export default function MonsterSection({
-  correctAnswer,
-  dropId,
-  setCorrectAnswer,
-}: MonsterSectionProps) {
-  const [monsters, setMonsters] = useState<MonsterType[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/monsters")
-      .then((response) => response.json())
-      .then((data) => {
-        setMonsters(
-          data.map((monster: MonsterAPI) => ({
-            id: monster.id,
-            description: monster.description,
-            name: monster.name,
-            image: `http://localhost:5000${monster.monster_neutral_image_url}`,
-            imageNeutral: `http://localhost:5000${monster.monster_neutral_image_url}`,
-            imageHappy: `http://localhost:5000${monster.monster_happy_image_url}`,
-            imageSad: `http://localhost:5000${monster.monster_sad_image_url}`,
-            materialType: monster.material_type,
-            monsterIcon: `http://localhost:5000${monster.monster_icon}`,
-            monsterColor: monster.monster_color,
-          })),
-        );
-      });
-  }, []);
-
+export default function MonsterSection({ monsters }: MonsterSectionProps) {
   const styles = {
     sectionContainer: {
       display: "flex",
@@ -46,42 +18,6 @@ export default function MonsterSection({
       minHeight: "0",
     },
   };
-
-  function returnToNeutralImage(dropId: string | null) {
-    setTimeout(() => {
-      const updateMonsterImage = monsters.map((monster) => {
-        if (monster.materialType === dropId) {
-          return { ...monster, image: monster.imageNeutral };
-        }
-        return monster;
-      });
-      setMonsters(updateMonsterImage);
-    }, 2000);
-  }
-
-  useEffect(() => {
-    if (correctAnswer === true) {
-      const updateMonsterImage = monsters.map((monster) => {
-        if (monster.materialType === dropId) {
-          return { ...monster, image: monster.imageHappy };
-        }
-        return monster;
-      });
-      setMonsters(updateMonsterImage);
-      setCorrectAnswer && setCorrectAnswer(null);
-      dropId ? returnToNeutralImage(dropId) : null;
-    } else if (correctAnswer === false) {
-      const updateMonsterImage = monsters.map((monster) => {
-        if (monster.materialType === dropId) {
-          return { ...monster, image: monster.imageSad };
-        }
-        return monster;
-      });
-      setMonsters(updateMonsterImage);
-      setCorrectAnswer && setCorrectAnswer(null);
-      dropId ? returnToNeutralImage(dropId) : null;
-    }
-  }, [correctAnswer, dropId]);
 
   return (
     <div style={styles.sectionContainer}>
--- src/pages/GameView.tsx
+++ src/pages/GameView.tsx
@@ -5,6 +5,8 @@ import ObjectSection from "../components/layout/object-section";
 import { DndContext } from "@dnd-kit/core";
 import { useEffect } from "react";
 import type { Item } from "../types/item";
+import type { MonsterAPI } from "../types/monster";
+import type { Monster as MonsterType } from "../types/monster";
 
 function GameView() {
   const [correctAnswer, setCorrectAnswer] = useState<boolean | null>(null);
@@ -15,23 +17,50 @@ function GameView() {
     {
       name: "Egg carton",
       image: "/images/egg-carton.png",
-      category: "carton",
+      category: "kartong",
     },
     {
       name: "Glass bottle",
       image: "/images/raspberry-juice-bottle.png",
-      category: "glass",
+      category: "glas",
     },
     {
       name: "Plastic bottle",
       image: "/images/water-bottle.png",
-      category: "plastic",
+      category: "plast",
     },
   ]);
+  const [monsters, setMonsters] = useState<MonsterType[]>([]);
+  const [isLoadingMonsters, setIsLoadingMonsters] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/monsters")
+      .then((response) => response.json())
+      .then((data) => {
+        setMonsters(
+          data.map((monster: MonsterAPI) => ({
+            id: monster.id,
+            description: monster.description,
+            name: monster.name,
+            image: `http://localhost:5000${monster.monster_neutral_image_url}`,
+            imageNeutral: `http://localhost:5000${monster.monster_neutral_image_url}`,
+            imageHappy: `http://localhost:5000${monster.monster_happy_image_url}`,
+            imageSad: `http://localhost:5000${monster.monster_sad_image_url}`,
+            materialType: monster.material_type,
+            monsterIcon: `http://localhost:5000${monster.monster_icon}`,
+            monsterColor: monster.monster_color,
+          })),
+        );
+        setIsLoadingMonsters(false);
+      });
+  }, []);
 
   const handleDragEnd = (event: any) => {
     const { active, over } = event;
     if (!over) return;
+
+    console.log("Active ID:", active.id);
+    console.log("Over ID:", over.id);
     if (active.id === over.id) {
       setDropId(over.id);
       setCorrectAnswer(true);
@@ -43,6 +72,44 @@ function GameView() {
     }
   };
 
+  function returnToNeutralImage(dropId: string | null) {
+    setTimeout(() => {
+      const updateMonsterImage = monsters.map((monster) => {
+        if (monster.materialType === dropId) {
+          return { ...monster, image: monster.imageNeutral };
+        }
+        return monster;
+      });
+      setMonsters(updateMonsterImage);
+    }, 2000);
+  }
+
+  useEffect(() => {
+    if (correctAnswer === true) {
+      const updateMonsterImage = monsters.map((monster) => {
+        if (monster.materialType === dropId) {
+          return { ...monster, image: monster.imageHappy };
+        }
+        return monster;
+      });
+      setMonsters(updateMonsterImage);
+      setCorrectAnswer && setCorrectAnswer(null);
+      dropId ? returnToNeutralImage(dropId) : null;
+    } else if (correctAnswer === false) {
+      const updateMonsterImage = monsters.map((monster) => {
+        if (monster.materialType === dropId) {
+          return { ...monster, image: monster.imageSad };
+        }
+        return monster;
+      });
+      setMonsters(updateMonsterImage);
+      setCorrectAnswer && setCorrectAnswer(null);
+      dropId ? returnToNeutralImage(dropId) : null;
+    }
+  }, [correctAnswer, dropId]);
+
+  console.log("correct answer ä", correctAnswer);
+
   useEffect(() => {
     const randomItem = items[Math.floor(Math.random() * items.length)];
     setRandomItem(randomItem);
@@ -52,11 +119,16 @@ function GameView() {
   return (
     <DndContext onDragEnd={handleDragEnd}>
       <Header />
-      <MonsterSection
-        correctAnswer={correctAnswer}
-        dropId={dropId}
-        setCorrectAnswer={setCorrectAnswer}
-      />
+      {isLoadingMonsters ? (
+        <p>Loading...</p>
+      ) : (
+        <MonsterSection
+          monsters={monsters}
+          correctAnswer={correctAnswer}
+          dropId={dropId}
+          setCorrectAnswer={setCorrectAnswer}
+        />
+      )}
       <ObjectSection randomItem={randomItem} isLoading={isLoading} />
     </DndContext>
   );
